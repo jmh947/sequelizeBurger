36f9ca3d808fb311d4aaff7490413fe11323972a
Jaclyn Hardy <jackihardy@yahoo.com>
2020-05-17 20:41:24 -0700

    connection working

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE sequelizeBurger_db;
+
+USE sequelizeBurger_db;
+
+CREATE TABLE burgers (
+id int auto_increment not null,
+name varchar(20) not null,
+devour boolean default false,
+primary key (id)
+
+);
